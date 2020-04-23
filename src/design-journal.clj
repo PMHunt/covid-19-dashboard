a31932165519a49ef39b717c5615f7603a03ced7
Phil Hunt <phil@deviantlogic.co.uk>
2020-04-23 10:24:41 +0100

    mock covid data for confirmed cases

--- src/design-journal.clj
+++ src/design-journal.clj
@@ -3,47 +3,7 @@
 
 (oz/start-server!)
 
-;; Oz examples
-
-(defn play-data [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
-(def line-plot
-  {:data {:values (play-data "monkey" "slipper" "broom")}
-   :encoding {:x {:field "time" :type "quantitative"}
-              :y {:field "quantity" :type "quantitative"}
-              :color {:field "item" :type "nominal"}}
-   :mark "line"})
-
-;; Render the plot
-(oz/view! line-plot)
-
-(def stacked-bar
-  {:data {:values (play-data "munchkin" "witch" "dog" "lion" "tiger" "bear")}
-   :mark "bar"
-   :encoding {:x {:field "time"
-                  :type "ordinal"}
-              :y {:aggregate "sum"
-                  :field "quantity"
-                  :type "quantitative"}
-              :color {:field "item"
-                      :type "nominal"}}})
-
-(oz/view! stacked-bar)
-
-
-;; load some example vega
-;; (this may only work from within a checkout of oz haven't checked)
-
-(require '[cheshire.core :as json])
-
-(def contour-plot (oz/load "contour-lines.vega.json"))
-
-(oz/view! contour-plot :mode :vega)
-
-;; end Oz examples
+;; Rework Oz examples into something for Covid data
 
 (defn mock-data-set
   "generates some mock data for each location name
@@ -57,3 +17,26 @@
      :location location
      :cases (+ (Math/pow (* day (count location)) 0.8)
                (rand-int (count location)))}))
+
+(def line-plot
+  {:data {:values (mock-data-set "England" "Wales" "Scotland" "Northern Ireland")}
+   :encoding {:x {:field "day" :type "quantitative"}
+              :y {:field "cases" :type "quantitative"}
+              :color {:field "location" :type "nominal"}}
+   :mark "line"})
+
+;; Render the plot
+(oz/view! line-plot)
+
+(def stacked-bar
+  {:data {:values (mock-data-set "England" "Wales" "Scotland" "Northern Ireland")}
+   :mark "bar"
+   :encoding {:x {:field "day"
+                  :type "ordinal"}
+              :y {:aggregate "sum"
+                  :field "cases"
+                  :type "quantitative"}
+              :color {:field "location"
+                      :type "nominal"}}})
+
+(oz/view! stacked-bar)
